b88b9e61a22169acecbf5475b0eb41bbc1b69c1c
petercsmith-x <petercsmith@xmos.com>
2025-06-19 15:54:40 +0100

    WIP: Add debug stuff

--- setup0.sh
+++ setup0.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 # Go to script location
-pushd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null
-rpi_setup_dir=$(pwd -P)
+rpi_setup_dir=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd -P)
 
 i2s_mode=
 xmos_device=
@@ -25,16 +24,40 @@ usage() {
         "The device-type is the XMOS device to set up. Valid types: $devices_display_string" >&2
 }
 
+error() {
+    echo -e "\033[0;31m[ERROR]\033[0m $1"
+}
+
+warn() {
+    if [[ $DEBUG -gt 0 ]]; then
+        echo -e "\033[0;33m[WARN]\033[0m $1"
+    fi
+}
+
+info() {
+    if [[ $DEBUG -gt 1 ]]; then
+        echo -e "\033[0;34m[INFO]\033[0m $1"
+    fi
+}
+
+trace() {
+    if [[ $DEBUG -gt 2 ]]; then
+        echo -e "\003[0;36m[TRACE]\033[0m $1"
+    fi
+}
+
 if [[ $# -eq 1 ]]; then
     xmos_device=$1
 
     # Check if the input device is valid and output usage if not
     if [[ ! " ${valid_xmos_devices[@]} " =~ " $xmos_device " ]]; then
-        echo "ERROR: $xmos_device is not a valid device type." >&2
+        error "$xmos_device is not a valid device type." >&2
         echo
         usage
         exit 1
     fi
+
+    info "Got device name: $xmos_device"
 else
     usage
     exit 1
@@ -53,58 +76,73 @@ case $xmos_device in
         ;;
     *)
         # This shouldn't happen as we've already validated the input device
-        echo "ERROR: Unknown XMOS device type $xmos_device." >&2
+        error "Unknown XMOS device type $xmos_device." >&2
         echo
         usage
         exit 1
         ;;
 esac
 
+info "i2s_mode: $i2s_mode, io_exp_and_dac_setup: $io_exp_and_dac_setup, asoundrc_template: $asoundrc_template"
+
 # Begin setting up RasPi
 rpi_config=/boot/config.txt
 
 # Disable built-in audio
 sudo sed -i '/^dtparam=audio=on$/ s/^/#/' "$rpi_config"
+info "Disabled built-in audio in $rpi_config."
 
 # Enable I2S devicetree
 sudo sed -i '/^#dtparam=i2s=on$/s/^#//' "$rpi_config"
+info "Enabled I2S devicetree in $rpi_config."
 
 # Enable I2C devicetree
 sudo raspi-config nonint do_i2c 1
 sudo raspi-config nonint do_i2c 0
+info "Toggled I2C devicetree."
 
 # Set I2C baudrate to 100k
 if ! grep -q '^dtparam=i2c_arm_baudrate=100000$' "$rpi_config"; then
     echo "dtparam=i2c_arm_baudrate=100000" | sudo tee -a "$rpi_config" >/dev/null
 fi
+info "Set I2C baud to 100k."
 
 # Enable SPI
 sudo raspi-config nonint do_spi 1
 sudo raspi-config nonint do_spi 0
+info "Enabled SPI."
 
 # Install required packages
+info "Attempting to install required packages: ${packages[*]}"
 for ((attempt=1; attempt <= max_install_attempts; attempt++)); do
     # Attempt to install all packages
     if sudo apt-get install -y ${packages[*]}; then
         break
     fi
+    
+    warn "Failed to install required packages, attempt $attempt / $max_install_attempts"
 
     # Increasingly sleep between attempts
     if [[ ! $attempt -eq $max_install_attempts ]]; then
+        trace "Sleeping for $((attempt * 2)) seconds..."
         sleep $((attempt * 2))
     fi
 done
 
+info "Checking required packages are installed."
+
 # Check for failed package installations
 for package in ${packages[@]}; do
+    trace "Checking $package..."
     if ! dpkg -s $package &>/dev/null; then
         failed_packages+=($package)
+        warn "Package $package failed to install."
     fi
 done
 
 # Output failed packages
 if [[ ${#failed_packages[@]} -gt 0 ]]; then
-    echo "ERROR: Failed to install the following packages after $max_install_attempts attempts:" >&2
+    error "Failed to install the following packages after $max_install_attempts attempts:" >&2
     printf ' - %s\n' "${failed_packages[@]}" >&2
     echo "Please check network connection and package names, then try again." >&2
     exit 1
@@ -113,72 +151,89 @@ fi
 
 # TODO: Test and make different configurations for devices
 # Install VocalFusion devicetree overlay
+info "Making and installing VocalFusion DTO."
 make -C $rpi_setup_dir/overlays install
 
 # Enable VocalFusion devicetree overlay now
+info "Enabling DTO now."
 sudo dtoverlay vocalfusion-device
 
 # TODO: Check out what this does
 # Copy the udev rules files if device is UA
 if [[ -n "$usb_mode" ]]; then
-  echo "Add UDEV rules for XMOS devices"
-  sudo cp $rpi_setup_dir/resources/99-xmos.rules /etc/udev/rules.d/
+    info "Adding UDEV rules for XMOS devices"
+    sudo cp $rpi_setup_dir/resources/99-xmos.rules /etc/udev/rules.d/
 fi
 
 # Move existing files to back up
 if [[ -e ~/.asoundrc ]]; then
-  chmod a+w ~/.asoundrc
-  cp ~/.asoundrc ~/.asoundrc.bak
+    info "Moving ~/.asoundrc to ~/.asoundrc.bak"
+    chmod a+w ~/.asoundrc
+    cp ~/.asoundrc ~/.asoundrc.bak
 fi
 
 if [[ -e /usr/share/alsa/pulse-alsa.conf ]]; then
-  sudo mv /usr/share/alsa/pulse-alsa.conf  /usr/share/alsa/pulse-alsa.conf.bak
+    info "Moving /usr/share/alsa/pulse-alsa.conf to /usr/share/alsa/pulse-alsa.conf.bak"
+    sudo mv /usr/share/alsa/pulse-alsa.conf /usr/share/alsa/pulse-alsa.conf.bak
 fi
 
 # Check XMOS device for asoundrc selection.
 if [[ -z "$asoundrc_template" ]]; then
-  echo "ERROR: sound card config not known for XMOS device $xmos_device." >&2
+  error "Sound card config not known for XMOS device $xmos_device." >&2
   exit 1
 fi
 
+# Copy asoundrc config to correct location
+info "Copying asoundrc: $asoundrc_template to ~/.asoundrc"
+cp $asoundrc_template ~/.asoundrc
+
 # Apply changes
+info "Applying .asoundrc changes."
 sudo /etc/init.d/alsa-utils restart
 
 if [[ -n $i2s_mode ]]; then
-  # Create the script to run after each reboot and make the soundcard available
-  i2s_setup_script=$rpi_setup_dir/resources/setup_i2s_${i2s_mode}.sh
-  rm -f $i2s_setup_script
+    # Create the script to run after each reboot and make the soundcard available
+    i2s_setup_script=$rpi_setup_dir/resources/setup_i2s_${i2s_mode}.sh
+    info "Creating I2S setup script: $i2s_setup_script."
+    info "Removing old script."
+    rm -f $i2s_setup_script
 
-  # Sometimes with Buster on RPi3 the SYNC bit in the I2S_CS_A_REG register is not set before the drivers are loaded
-  # According to section 8.8 of https://cs140e.sergio.bz/docs/BCM2837-ARM-Peripherals.pdf
-  # this bit is set after 2 PCM clocks have occurred.
-  # To avoid this issue we add a 1-second delay before the drivers are loaded
-  echo "sleep 1"  >> $i2s_setup_script
+    # Sometimes with Buster on RPi3 the SYNC bit in the I2S_CS_A_REG register is not set before the drivers are loaded
+    # According to section 8.8 of https://cs140e.sergio.bz/docs/BCM2837-ARM-Peripherals.pdf
+    # this bit is set after 2 PCM clocks have occurred.
+    # To avoid this issue we add a 1-second delay before the drivers are loaded
+    echo "sleep 1"  >> $i2s_setup_script
 
-  echo "# Run Alsa at startup so that alsamixer configures" >> $i2s_setup_script	
-  echo "arecord -d 1 > /dev/null 2>&1"                      >> $i2s_setup_script	
-  echo "aplay dummy > /dev/null 2>&1"                       >> $i2s_setup_script
+    echo "# Run Alsa at startup so that alsamixer configures" >> $i2s_setup_script	
+    echo "arecord -d 1 > /dev/null 2>&1"                      >> $i2s_setup_script	
+    echo "aplay dummy > /dev/null 2>&1"                       >> $i2s_setup_script
 
-  if [[ "$i2s_mode" = "master" ]]; then
-    echo "# Preconfigure i2s clocks to 48kHz"               >> $i2s_setup_script
-    # wait a bit as it doesn't work otherwise, this is probably caused
-    # by the same process that is deleting .asoundrc
-    echo "sleep 15"                                         >> $i2s_setup_script
-    echo "arecord -Dhw:CARD=Device,DEV=0 -c2 -fS32_LE -r48000 -s1 /dev/null" >> $i2s_setup_script
-    echo "sudo $rpi_setup_dir/resources/clk_dac_setup/setup_bclk" >> $i2s_setup_script
-  fi
+    if [[ "$i2s_mode" = "master" ]]; then
+        info "I2S mode is $i2s_mode, adding clock preconfiguration to setup script..."
+
+        echo "# Preconfigure i2s clocks to 48kHz"               >> $i2s_setup_script
+        # wait a bit as it doesn't work otherwise, this is probably caused
+        # by the same process that is deleting .asoundrc
+        echo "sleep 15"                                         >> $i2s_setup_script
+        echo "arecord -Dhw:CARD=Device,DEV=0 -c2 -fS32_LE -r48000 -s1 /dev/null" >> $i2s_setup_script
+        echo "sudo $rpi_setup_dir/resources/clk_dac_setup/setup_bclk" >> $i2s_setup_script
+    fi
 fi
 
 if [[ -n "$io_exp_and_dac_setup" ]]; then
   # Build setup binary
+  info "Building clock and DAC setup binaries..."
   make -C $rpi_setup_dir/resources/clk_dac_setup/
 
   # Create DAC and CLK setup script
   dac_and_clks_script=$rpi_setup_dir/resources/init_dac_and_clks.sh
+  info "Creating DAC and CLK setup script: $dac_and_clks_script."
+  info "Removing old script."
   rm -f $dac_and_clks_script
 
   # Configure the clocks only if RaspberryPi is configured as I2S master
   if [[ "$i2s_mode" = "master" ]]; then
+    info "I2S mode is $i2s_mode, adding clock configuration to script."
     echo "sudo $rpi_setup_dir/resources/clk_dac_setup/setup_mclk"   >> $dac_and_clks_script
     echo "sudo $rpi_setup_dir/resources/clk_dac_setup/setup_bclk"   >> $dac_and_clks_script
   fi
@@ -190,17 +245,21 @@ fi
 
 if [[ -n "$io_exp_and_dac_setup" ]]; then
   audacity_script=$rpi_setup_dir/resources/run_audacity.sh
+  info "Creating Audacity script $audacity_script."
+  info "Removing old script."
   rm -f $audacity_script
 
   echo "#!/usr/bin/env bash" >> $audacity_script
   echo "/usr/bin/audacity &" >> $audacity_script
   echo "sleep 5" >> $audacity_script
   if [[ "$i2s_mode" = "master" ]]; then
+    info "I2S mode is $i2s_mode, adding clock configuration to script."
     echo "sudo $rpi_setup_dir/resources/clk_dac_setup/setup_bclk >> /dev/null" >> $audacity_script
   fi
   sudo chmod +x $audacity_script
 
   # /usr/local/bin before /usr/bin in PATH
+  info "Moving script from $audacity_script to /usr/local/bin/audacity"
   sudo mv $audacity_script /usr/local/bin/audacity
 fi
 
@@ -212,6 +271,7 @@ elif [[ -n "$i2s_mode" ]]; then
     crontab_file="${crontab_file}_i2s_${i2s_mode}"
 fi
 
+info "Removing crontab file $crontab_file"
 rm -f $crontab_file
 
 # Setup the crontab to restart I2S at reboot
@@ -230,7 +290,10 @@ fi
 # https://forums.raspberrypi.com/viewtopic.php?t=295008
 echo "@reboot sleep 20 && cp $asoundrc_template ~/.asoundrc" >> $crontab_file
 
+info "New crontab file:\n$(<$crontab_file)"
+
 # Update crontab
+info "Updating crontab."
 crontab $crontab_file
 
 echo "To enable all interfaces, this Raspberry Pi must be rebooted."
